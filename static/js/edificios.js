203bfdafd926c8064d3cbe94df18d00a090ea784
pablopavon23 <p.pavon23695@gmail.com>
2018-12-05 10:48:54 +0100

    V.05/12/2018

---
+++ static/js/edificios.js
@@ -0,0 +1,18 @@
+var medidas =
+{"ED70_1": {"1": {"CO2 (ppp)": 553.628, "ABIERTO/CERRADO": 0, "gps": [-3.7287271, 40.45471573]},
+            "2": {"CO2 (ppp)": 575.542, "TMP-aire (c)": 27.7478, "ABIERTO/CERRADO": 0, "gps": [-3.72868991, 40.45471573]},
+            "3": {"CO2 (ppp)": 776.035, "TMP-aire (c)": 23.3646, "ABIERTO/CERRADO": 0, "gps": [-3.728652, 40.45471954]},
+            "4": {"CO2 (ppp)": 462.749, "TMP-aire (c)": 24.5945, "HUMEDAD (%)": 22.5998, "ABIERTO/CERRADO": 0.0, "gps": [-3.7286551, 40.45475006]},
+            "5": {"ABIERTO/CERRADO": 0, "TMP-superficie (c)": 22.9343, "gps": [-3.72866201, 40.45479965]},
+            "6": {"ABIERTO/CERRADO": 0, "gps": [-3.72861505, 40.45477676]},
+            "7": {"ABIERTO/CERRADO": 0, "gps": [-3.72861695, 40.45475388]},
+            "8": {"ABIERTO/CERRADO": 0, "gps": [-3.72861409, 40.45472336]}},
+"ED70_2": {"9": {"CO2 (ppp)": 591.217, "ABIERTO/CERRADO": 0, "HUMEDAD (%)": 20.708, "gps": [-3.728577, 40.454726]},
+           "10": {"TMP-superficie (c)": 25.1319, "TMP-aire (c)": 23.7028, "ABIERTO/CERRADO": 0, "gps": [-3.72857, 40.454675]},
+           "11": {"ABIERTO/CERRADO": 1.0, "gps": [-3.728538, 40.454729]},
+           "12": {"CO2 (ppp)": -499.976, "TMP-aire (c)": 22.8837, "ABIERTO/CERRADO": 0, "gps": [-3.728575, 40.45478]},
+           "13": {"CO2 (ppp)": 483.029, "TMP-aire (c)": 23.9931, "ABIERTO/CERRADO": 0, "gps": [-3.728579, 40.454756]},
+           "14": {"CO2 (ppp)": 352.611, "TMP-aire (c)": 23.2252, "ABIERTO/CERRADO": 0, "gps": [-3.728537, 40.454783]},
+           "15": {"CO2 (ppp)": 508.465, "TMP-aire (c)": 24.9166, "ABIERTO/CERRADO": 0, "gps": [-3.728541, 40.454759]},
+           "16": {"CO2 (ppp)": 782.501, "ABIERTO/CERRADO": 1.0, "gps": [-3.728505, 40.454762]},
+           "17": {"ABIERTO/CERRADO": 0, "gps": [-3.72844, 40.454752]}}};
