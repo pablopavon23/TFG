b764cbe77fd0745dd5be02255e25ef73e92d957f
Pablo Pavon Pavon <ppavon@alpha21.aulas.gsyc.urjc.es>
2018-12-20 13:43:06 +0100

    V.20/12/2018

--- static/js/leaflet_Clinica_Fuenlabrada.js
+++ static/js/leaflet_Clinica_Fuenlabrada.js
@@ -1,3 +1,4 @@
+// AÑADIMOS BLOQUE PARA REPRESENTAR EL PUNTO DENTRO DEL MAPA
 var map = L.map('map').setView([40.2770,-3.7970],15);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',maxZoom: 18}).addTo(map);
 L.control.scale().addTo(map);
