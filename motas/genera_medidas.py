4a86db0536389f7c98d4271af310ac076e0d829b
pablopavon23 <p.pavon23695@gmail.com>
2019-11-28 23:23:05 +0100

    V.28/11/2019

---
+++ motas/genera_medidas.py
@@ -0,0 +1,224 @@
+from datetime import datetime, date, time, timedelta
+import calendar
+# Importamos el módulo predefinido para creacón de ficheros JSON:
+import json
+import os
+# Importamos los modelos:
+from .models import *
+
+# ---------------------------------------------------------------------------------------------------------
+
+def get_CF_info():
+    # A modo de prueba saco la información
+    todas_motas = ClinicaFuenlabrada1Motas.objects.all()
+
+    i = 0
+    dicts = []
+    while i < len(todas_motas):
+        dict = {}
+        dict['id'] = todas_motas[i].id_mota
+        dict['planta'] = planta_mota(todas_motas[i].id_mota)
+        dict['sensores'] = todas_motas[i].num_sensores
+        lista_sensores = ClinicaFuenlabrada1Sensores.objects.filter(id_mota=todas_motas[i].id_mota) # Busco los sensores asociados a esa mota
+        dict['tipsen'] = list(lista_sensores)
+        dicts.append(dict)
+        i += 1
+
+
+
+    return dicts
+
+# ---------------------------------------------------------------------------------------------------------
+
+def get_CF_info_json():
+    # A modo de prueba saco la información
+    todas_motas = ClinicaFuenlabrada1Motas.objects.all()
+
+    i = 0
+    dicts = []
+    while i < len(todas_motas):
+        dict = {}
+        sensor_type = []
+        dict['id'] = todas_motas[i].id_mota
+        dict['planta'] = planta_mota(todas_motas[i].id_mota)
+        dict['sensores'] = todas_motas[i].num_sensores
+        lista_sensores = ClinicaFuenlabrada1Sensores.objects.filter(id_mota=todas_motas[i].id_mota) # Busco los sensores asociados a esa mota
+        for medida in lista_sensores:
+            sensor_type.append(medida.tipo_medida)
+        dict['tipsen'] = sensor_type
+        dicts.append(dict)
+        i += 1
+
+    # Hago el JSON que contiene los datos de medidas.
+    generate_json("relacion.json",dicts)
+
+# ---------------------------------------------------------------------------------------------------------
+
+# Funcion que me ayuda a poder meter un datetime en un JSON:
+def myconverter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+
+# ---------------------------------------------------------------------------------------------------------
+
+def get_medidas_true():
+    # Hago una query a la tabla de mi base de datos donde estan las medidas:
+    # todas_medidas = ClinicaFuenlabrada1Medidas.objects.all() -- Cuidado porque esto podría estar ejecutandose la vida
+    ultimas_medidas = ClinicaFuenlabrada1Medidas.objects.all()[:10]
+
+    # Llamo a la funcion que me carga la relacion de motas para saber que tipos de sensor tiene cada mota:
+    motas_rel = get_CF_info()
+
+    """
+    Ahora tengo que hacer un procedimiento que:
+    1. Busque en ClinicaFuenlabrada1Medidas.objects.filter(id_mota=X).filter(tipo_medida=XXX).order_by('hora')[:10]
+    2. Luego necesito:
+        - id_mota, que la iré cogiendo de motas_rel con un bucle
+        - tipo_medida, que tendré que conseguir buscando para una id_mota en motas_rel los tipos de medidas que tiene
+    """
+    medidas_definitivas = []
+    for dicts in motas_rel:
+        idm = dicts['id']
+        current_dic = {} # aqui guardo el diccionario acutal que luego inserto en la lista
+        current_dic['id_mota'] = idm    # tambien me valdria dicts['id']
+        current_dic['planta'] = dicts['planta']
+        current_dic['sensores'] = dicts['sensores']
+        current_dic['gps'] = gps_mota(idm)
+        # print("Los diccionarios son: "+str(dicts))
+        med_types = []
+        for tipo in dicts['tipsen']:
+            if not tipo.tipo_medida in med_types:
+                current_med = []    # lista de medidas
+                current_hora = []   # lista de horas
+                print("El tipo es:"+str(tipo.tipo_medida))
+                tip_actual = tipo.tipo_medida
+                # print("El tipo ahora es: "+str(tip_actual))
+                med_types.append(tip_actual)
+                # Ahora ya obtengo para esa mota sus diferentes tipos de medidas
+                med_actual = ClinicaFuenlabrada1Medidas.objects.filter(id_mota=idm).filter(tipo_medida=tip_actual).order_by('hora')[:2]
+                # print("La medida actual es: "+str(med_actual))
+                """
+                Voy a hacer aqui la prueba de meterlo en el JSON:
+                Meto una entrada por cada medida, algo del estilo:
+                [
+                {'id_mota':1,'planta':3,'sensores':2,'gps':[-3.78...,40.78...],'HUMEDAD (%)':[65],'TMP-aire (c)': [27],'hora':16:16:00+00:00},
+                otro diccionario,
+                otro,
+                ...
+                Lo primero ya lo tengo arriba solo me queda 'HUMEDAD (%)':[15 objects],'TMP-aire (c)': [15 objects] o lo que sea
+                El object ese de lo unico que quiero de ahi es medida y hora.
+                ]
+                """
+                # med_actual es una serie de QuerySet, debo acceder al campo medida de cada uno.
+                # tip_actual_mapeado = map_tipo(tip_actual)   # Necesario para acceder luego en el template
+                for medidas in med_actual:
+                    current_med.append(medidas.medida)
+                    current_hora.append(medidas.hora)
+                    # current_dic[tip_actual_mapeado] = medidas.medida
+                    # current_dic['hora'] = medidas.hora
+
+                tip_actual_mapeado = map_tipo(tip_actual)   # Necesario para acceder luego en el template
+                i = 1
+                while i < len(current_med):
+                    current_dic[tip_actual_mapeado] = current_med[i]
+                    current_dic['hora'] = current_hora[i]
+                    # Añado ese diccionario a la lista
+                    medidas_definitivas.append(current_dic)
+                    i+=1
+
+
+        # print("LA MOTA "+str(idm)+" :TIENES: "+str(med_types))
+        # Añado ese diccionario a la lista
+        # medidas_definitivas.append(current_dic)
+
+    # Hago el JSON que contiene los datos de medidas.
+    generate_json("medidas.json",medidas_definitivas)
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+def generate_json(file_name,info_dicts):
+    # dir = 'C:/Pruebas'  # También es válido 'C:\\Pruebas' o r'C:\Pruebas' -- Esto por si quiero meterlo en un directorio distinto al que me encuentro
+    dir = '/home/ppp23/Escritorio/TFG/data'
+    # file_name = "data.json"
+
+    # with open(file_name, 'w') as file:
+    with open(os.path.join(dir, file_name), 'w') as file: #-- Esto por si quiero meterlo en un directorio distinto al que me encuentro
+        json.dump(info_dicts, file, default=myconverter)
+
+# ---------------------------------------------------------------------------------------------------------
+
+# Recibe como argumento el id_mota y según este podemos decir en qué planta está:
+def planta_mota(mota):
+
+    if mota in [13,18]:
+        planta = 1
+    elif mota in [12,14]:
+        planta = 2
+    elif mota in [1,2,3,5,6,7,15]:
+        planta = 3
+    elif mota in [8,9,10,11,16,17]:
+        planta = 4
+    else:
+        planta = "no data"
+
+    return planta
+
+# ---------------------------------------------------------------------------------------------------------
+
+# Recibe como argumento el id_mota y según este podemos decir en qué coordenadas tiene:
+# Se podria sustituir por un diccionario creo, al igual que el de la planta.
+def gps_mota(mota):
+
+    if mota == 1:
+        gps = [-3.7973980744918094,40.277264074863325]
+    elif mota == 2:
+        gps = [-3.796926005621316,40.277124106016885]
+    elif mota == 3:
+        gps = [-3.7973263254056633,40.277321167285066]
+    elif mota == 5:
+        gps = [-3.796928017273842,40.277049210358825]
+    elif mota == 6:
+        gps = [-3.797550960372604,40.27708277013147]
+    elif mota == 7:
+        gps = [-3.796976967647737,40.277301522572856]
+    elif mota == 8:
+        gps = [-3.797379969547677,40.277267348802354]
+    elif mota == 9:
+        gps = [-3.7969575215968234,40.27713249590113]
+    elif mota == 10:
+        gps = [-3.7975536425734617,40.277079496013215]
+    elif mota == 11:
+        gps = [-3.7968971718938747,40.27729681586601]
+    elif mota == 12:
+        gps = [-3.79707486829426,40.27713965824617]
+    elif mota == 13:
+        gps = [-3.7973776226223492,40.27726356322137]
+    elif mota == 14:
+        gps = [-3.797374605270221,40.27725445708464]
+    elif mota == 15:
+        gps = [-3.797331689784727,40.277073561654646]
+    elif mota == 16:
+        gps = [-3.7969903786510883,40.277270418288424]
+    elif mota == 17:
+        gps = [-3.797427578755986,40.277092183247134]
+    elif mota == 18:
+        gps = [-3.796971603319321,40.2771160230848]
+    else:
+        gps = []
+
+    return gps
+
+# ------------------------------------------------------------------------------------------------
+
+def map_tipo(tipo):
+    if tipo == 'TMP-aire (c)':
+        tipo_map = 'TMPaire'
+    elif tipo == 'HUMEDAD (%)':
+        tipo_map = 'HUMEDAD'
+    elif tipo == 'CO2 (ppp)':
+        tipo_map = 'CO2'
+    elif tipo == 'ABIERTO/CERRADO':
+        tipo_map = 'ABICER'
+
+    return tipo_map
--- motas/views.py
+++ motas/views.py
@@ -11,10 +11,12 @@ from django.template.context_processors import csrf
 from django.contrib.auth import logout, authenticate, login
 # Importamos los modelos:
 from .models import *
-# Importamos el lector de la SQL:
-# from .parser_SQL import executeScriptsFromFile
 # Importamos el archivo y función que reside en el mismo para cargar medidas:
-from .load_medidas import get_medidas, get_CF_info, get_medidas_true;
+from .load_medidas import get_medidas;
+from .genera_medidas import get_CF_info, get_medidas_true;
+# Para trabajar con JSON:
+import json
+import os
 
 # Create your views here.
 
@@ -33,23 +35,19 @@ def users_pages(user):
 # y redirigiendo si es un POST
 @csrf_exempt
 def user_login(request):
-    # print(request)
-    # Para imprimir la url sobre la que se hace la peticion
-    # print(request.path)
-    # print(request.method)
+
+    # Proceso la solicitud
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
-        #Si es un usuario valido me devuelve el nombre,sino da 'none'
 
-        if user is None: #Es usuario no valido
+        if user is None: # Es usuario no válido
             return redirect('/register')
-        else:
+        else: # Es usuario válido
             login(request,user)
             new_url = '/'+str(user)+'/index'
             return redirect(new_url)
-
     else:
         contexto = {'Login':"Hola"}
         return render(request,'login.html',contexto)
@@ -57,8 +55,6 @@ def user_login(request):
 # ------------------------------------------------------------------------------
 # Hacemos logout y se nos redirige a la pagina principal -> login
 def user_logout(request,peticion):
-    # print(request)  # <WSGIRequest: GET '/urjc/logout'>
-    # print(peticion) # urjc
     logout(request)
     return redirect('/')
 
@@ -75,77 +71,61 @@ def register(request):
 def index(request, peticion):
     # Consigo el usuario que ha accedido al portal
     user = request.user
-    # print("User is: "+str(user))
 
     # Llamo a la función que me dirá a que edificios tiene acceso ese usuario
     pages_info_user = users_pages(str(user))
 
     # Esto genera los JSON
-    test = get_medidas_true()
+    get_medidas_true()
 
     # Lo inroduzco como contexto para representarlo despues
     contexto = {'Builds':pages_info_user, 'User': user}
-
     return render(request,'index.html',contexto)
 
 # ------------------------------------------------------------------------------
+# Funcion que recibe como argumento el tipo de medida y los datos y devuelve un boolean indicando si
+# hay valores anómalos o no.
+def check_medida(tipo,medidas_test):
 
+    if tipo == 'temperatura':
+        check_tipo = "TMPaire"
+    elif tipo == 'dioxido':
+        check_tipo = "CO2"
+    elif tipo == 'humedad':
+        check_tipo = "HUMEDAD"
+
+    # Comprobamos cada medida:
+    muestras = []
+    current = []
+    for medidas in medidas_test:    # medidas es cada uno de los diccionarios
+        current = medidas.get(check_tipo)
+        if (current != None):   # Si no tenemos medida no se añade
+            for tupla in current:
+                muestras.append(tupla[0])
+
+    # Ordeno las muestras para que queden de min. valor a max. valor
+    muestras = sorted(muestras)
+    # Compruebo si hay (o no) alerta
+    if (muestras[0] < 0.0) or (muestras[len(muestras)-1] > 50.0):
+        alertar = True
+    else:
+        alertar = False
+
+    return alertar
+
+# ------------------------------------------------------------------------------
+#     """ La función aqui es devolver si hay que alertar o no"""
 def check_values(medidas_test):
-    # Comprobamos que no haya valores anómalos en temperaturas: ----------------
-    temperaturas = []
-    for medidas in medidas_test:    # medidas es cada uno de los diccionarios
-        print("La temperatura: "+str(medidas.get("TMPaire")))
-        if ((medidas.get("TMPaire")) != None):   # Si no tenemos medida no se añade
-            temperaturas.append(medidas.get("TMPaire"))
-
-    print("Temperaturas: "+str(temperaturas))
-    temperaturas = sorted(temperaturas)
-    print("Las motas despues son: "+str(temperaturas))    # Agrupamos los temperatura en orden ascendente
-
-    if (temperaturas[0] < 0.0) or (temperaturas[len(temperaturas)-1] > 50.0):
-        alertar_TMP = True
-    else:
-        alertar_TMP = False
-    # --------------------------------------------------------------------------
-
-    # Comprobamos que no haya valores anómalos en co2: ----------------
-    dioxido = []
-    for medidas in medidas_test:    # medidas es cada uno de los diccionarios
-        if ((medidas.get("CO2")) != None):   # Si no tenemos medida no se añade
-            dioxido.append(medidas.get("CO2"))
-
-    print("c02: "+str(dioxido))
-    dioxido = sorted(dioxido)
-    print("c02 despues son: "+str(dioxido))    # Agrupamos los temperatura en orden ascendente
-
-    if (dioxido[0] < 0.0) or (dioxido[len(dioxido)-1] > 810.0):
-        alertar_CO = True
-    else:
-        alertar_CO = False
-    # --------------------------------------------------------------------------
-
-    # Comprobamos que no haya valores anómalos en humedades: ----------------
-    humedades = []
-    for medidas in medidas_test:    # medidas es cada uno de los diccionarios
-        if ((medidas.get("HUMEDAD")) != None):   # Si no tenemos medida no se añade
-            humedades.append(medidas.get("HUMEDAD"))
-
-    print("humedades: "+str(humedades))
-    humedades = sorted(humedades)
-    print("humedades despues son: "+str(humedades))    # Agrupamos los temperatura en orden ascendente
-
-    if (humedades[0] < 0.0) or (humedades[len(humedades)-1] > 85.0):
-        alertar_HU = True
-    else:
-        alertar_HU = False
-    # --------------------------------------------------------------------------
+    # Invoco a la funcion que me indica si una medida cualquiera contiene valor anomalo o no
+    alertar_TMP = check_medida('temperatura',medidas_test)
+    alertar_CO = check_medida('dioxido',medidas_test)
+    alertar_HU = check_medida('humedad',medidas_test)
 
     if (not alertar_TMP) and (not alertar_CO) and (not alertar_HU):
         alertar = False
     else:
         alertar = True
 
-    """ La función de esta función es devolver si hay que alertar o no"""
     return alertar
 
 # ------------------------------------------------------------------------------
@@ -153,14 +133,45 @@ def check_values(medidas_test):
 def insert_idMota(medidas_test):
     cant_motas = []   # Inicializo la lista donde guardare mota1, mota2...
     for medidas in medidas_test:    # medidas es cada uno de los diccionarios
-        id_mota = medidas.get("mota")  # esto me permite saber que id de mota tengo
-        print("Mota: "+str(id_mota)) #ASi obtengo la mota a la que pertenece
-        if not str(id_mota) in cant_motas:
+        id_mota = medidas.get("id_mota")  # esto me permite saber que id de mota tengo
+        if not str(id_mota) in cant_motas:  # asi me aseguro no guardar dos veces la misma mota.
             cant_motas.append(str(id_mota))
 
-    cant_motas = sorted(cant_motas)
+    # Ordeno las motas para que se muestren en orden
+    cant_motas = sorted(cant_motas,key=int)
 
     return cant_motas
+
+# ------------------------------------------------------------------------------
+# Aquí cargo la información del JSON
+def get_data():
+    dir = '/home/ppp23/Escritorio/TFG/data'
+    with open(os.path.join(dir, 'medidas.json')) as file: #-- Esto por si quiero meterlo en un directorio distinto al que me encuentro
+        data = json.load(file)
+
+    return data
+
+# ------------------------------------------------------------------------------
+# Proceso solicitud del POST recibido de slices pidiendo mota y medidas concretas
+def procesolicitud(mota,medida,medidas_test):
+    esTMP = False
+    esHUM = False
+    esCO = False
+
+    # Segun la medida que me pidan solicitare luego en el JavaScript una medida u otra:
+    if medida == 'Temperatura':
+        esTMP = True
+    elif medida == 'Humedad':
+        esHUM = True
+    elif medida == 'CO2':
+        esCO = True
+
+    info_mota = []  # aqui almacenare solo las que el id de mota coincida con el pedido
+    for medidas in medidas_test:    # medidas es cada uno de los diccionarios
+        if str(medidas.get("id_mota")) == str(mota):    # comparo id como str sino no reconoce al ser str == int
+            info_mota.append(medidas)
+
+    return info_mota
 
 # ------------------------------------------------------------------------------
 
@@ -168,17 +179,9 @@ def insert_idMota(medidas_test):
 def slices(request, peticion):
     # Consigo el usuario que ha accedido al portal
     user = request.user
-    # print("User is: "+str(user))
-
-    # -------- Esto es para conseguir saber que slice me estan pidiendo ----
-    url = request.path
+    # Esto es para conseguir saber que slice me estan pidiendo
     url_slices = url.split('/')[2] # Obtengo slices_ED_70, slices_Clinica_Fuenlabrada y slices_Lece
-    print(url_slices)
 
-    # -------- Esto es para conseguir saber que slice me estan pidiendo ----
-    url = request.path
-    url_slices = url.split('/')[2] # Obtengo slices_ED_70, slices_Clinica_Fuenlabrada y slices_Lece
-    print("Solicitan la slice: "+url_slices)
     # Meto el nombre del edificio para personalizarlo más
     if (url_slices == "slices_ED_70"):
         nombre_ed = "Edificio 70, Ciemat"
@@ -187,37 +190,18 @@ def slices(request, peticion):
     elif (url_slices == "slices_Lece"):
         nombre_ed = "Plataforma Solar de Andalucía, Almería"
 
-    medidas_test = get_medidas(url_slices) # medidas_test es la lista de diccionarios
-    # La funcion get_medidas me devuelve la lista con las medidas para un edificio concreto
+    # Consigo los datos:
+    medidas_test = get_data()
 
-# --------- Copio tal cual de tables
-    # Hasta aquiiiiiii se podria exportar a una funcion introduce id_mota --> ya hecho solo llamo a la funcion
+    # Esto me permite mostrar el menú desplegable
     cant_motas = insert_idMota(medidas_test)
 
-    esTMP = False
-    esHUM = False
-    esCO = False
     if request.method == 'POST':
         mota_concreta = request.POST['mota']    # Averiguo sobre que mota en concreto solicitan info
-        print("La mota es: "+mota_concreta)
         medida_concreta = request.POST['medidatipo']    # Averiguo sobre que medida en concreto solicitan info
-        print("La medida es: "+medida_concreta)
-        # Segun la medida que me pidan solicitare luego en el JavaScript una medida u otra:
-        if medida_concreta == 'Temperatura':
-            esTMP = True
-        elif medida_concreta == 'Humedad':
-            esHUM = True
-        elif medida_concreta == 'CO2':
-            esCO = True
-        info_mota = []  # aqui almacenare solo las que el id de mota coincida con el pedido
-        for medidas in medidas_test:    # medidas es cada uno de los diccionarios
-            print("La verdadera: "+str(medidas.get("mota")))
-            if str(medidas.get("mota")) == str(mota_concreta):    # comparo id como str sino no reconoce al ser str == int
-                info_mota.append(medidas)
-        medidas_send = info_mota    # si es un POST mando las medidas de una mota concreta
+        medidas_send = procesolicitud(mota_concreta,medida_concreta,medidas_test)
     else:
         medidas_send = medidas_test # si es un GET mando todas las medidas tal cual las recibo de get_medidas()
-# -- Hasta aqui copio tal cual de tables
 
     # Llamo a la función que me dirá a que edificios tiene acceso ese usuario
     pages_info_user = users_pages(str(user))
@@ -248,8 +232,10 @@ def tables(request, peticion):
     url_tables = url.split('/')[2] # Obtengo tables_ED_70, tables_Clinica_Fuenlabrada y tables_Lece
     print("Solicitan la tabla: "+url_tables)
 
-    medidas_test = get_medidas(url_tables) # medidas_test es la lista de diccionarios
-
+    # medidas_test = get_medidas(url_tables) # medidas_test es la lista de diccionarios
+    # Consigo los datos:
+    medidas_test = get_data()
+    
     # Des de aquiiiiiiiiiii
     # Hasta aquiiiiiii se podria exportar a una funcion introduce id_mota --> ya hecho solo llamo a la funcion
     cant_motas = insert_idMota(medidas_test)
@@ -261,8 +247,8 @@ def tables(request, peticion):
         # medidas_test = get_medidas(url_tables) # medidas_test es la lista de diccionarios
         info_mota = []  # aqui almacenare solo las que el id de mota coincida con el pedido
         for medidas in medidas_test:    # medidas es cada uno de los diccionarios
-            print("La verdadera: "+str(medidas.get("mota")))
-            if str(medidas.get("mota")) == str(mota_concreta):    # comparo id como str sino no reconoce al ser str == int
+            print("La verdadera: "+str(medidas.get("id_mota")))
+            if str(medidas.get("id_mota")) == str(mota_concreta):    # comparo id como str sino no reconoce al ser str == int
                 info_mota.append(medidas)
         medidas_send = info_mota    # si es un POST mando las medidas de una mota concreta
     else:
